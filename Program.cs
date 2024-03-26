0045bf1f8603850a161f7a39fb9128580477f301
mrpingu98 <salh98manvir@gmail.com>
2024-03-26 11:37:03 +0000

    Changing swagger configuration

--- Program.cs
+++ Program.cs
@@ -65,11 +65,11 @@ app.UseCors(options =>
 });
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
+//if (app.Environment.IsDevelopment())
+//{
     app.UseSwagger();
     app.UseSwaggerUI();
-}
+//}
 
 app.UseHttpsRedirection();
 
